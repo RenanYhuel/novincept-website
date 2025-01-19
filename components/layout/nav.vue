7de7ab1dfa181a3c2a97905c5d016628fcefb8bc
FrostBlack <130504630+WebFrostBlack@users.noreply.github.com>
2025-01-19 09:53:08 +0100

    fix nav
    Fix not load css

--- components/layout/nav.vue
+++ components/layout/nav.vue
@@ -43,15 +43,9 @@
     nav {
         width: 1000px;
         height: 70%;
-        background-color: red;
         display: flex;
         justify-content: center;
-        align-items: center;
-        border-radius: 18px;
-        border: 1px solid var(--Border-border, #E4E4E7);
-        background: var(--Background-default, #F6F6F6);
-        box-shadow: 0px 15px 60px 17px #ffffff;
-        
+        align-items: center;       
     }
 
     nav ul {
@@ -61,6 +55,10 @@
         padding: 0;
         margin: 0;
         list-style: none;
+        border-radius: 18px;
+        border: 1px solid var(--Border-border, #E4E4E7);
+        background: var(--Background-default, #F6F6F6);
+        box-shadow: 0px 15px 60px 17px #ffffff;
     }
 
     nav ul li {
