e7771936ac80a86324e0f33545bc7755fd655b20
Renan Yhuel <renan.yhuel@gmail.com>
2025-02-08 18:28:17 +0100

    feat/section : add the fourth section with jobs to the About page

---
+++ components/about/Frame4.vue
@@ -0,0 +1,342 @@
+<template>
+    <div class="about-frame4-container">
+        <div class="about-frame4">
+            <div class="about-frame4-header">
+                <div class="about-frame4-header-title">
+                    Des <span class="about-frame4-header-title-highlight">postes</span> pour tout le monde ! 
+                </div>
+                <div class="about-frame4-header-subtitle">
+                    Sit ipsum ipsum consequat veniam reprehenderit adipisicing culpa excepteur consequat nostrud voluptate commodo occaecat. Dolore elit minim dolor anim ullamco ullamco in et mollit qui.
+                </div>
+            </div>
+            <div class="about-frame4-content">
+               <div class="about-frame4-content-title">
+                    Développement web
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">Développeur Front-End</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">Développeur Back-End</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">Développeur Front-End</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">Développeur Back-End</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-title">
+                    Design
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">UI/UX Designer</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">Brand Designer</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">Motion Designer</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+               <div class="about-frame4-content-card">
+                    <div class="about-frame4-content-card-header">
+                        <div class="about-frame4-content-card-header-title">UI/UX Designer</div>
+                        <div class="about-frame4-content-card-header-subtitle">A distance, en freelance</div>
+                    </div>
+                    <div class="about-frame4-content-card-link">
+                        <div class="about-frame4-content-card-link-text">Postuler à l’offre</div>
+                        <div class="about-frame4-content-card-link-icon">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39663 4.69789C7.52319 4.57149 7.69475 4.50049 7.87362 4.50049C8.0525 4.50049 8.22406 4.57149 8.35062 4.69789L12.1756 8.52289C12.302 8.64946 12.373 8.82102 12.373 8.99989C12.373 9.17877 12.302 9.35033 12.1756 9.47689L8.35062 13.3019C8.22267 13.4211 8.05343 13.486 7.87855 13.483C7.70368 13.4799 7.53684 13.409 7.41317 13.2854C7.28949 13.1617 7.21865 12.9948 7.21557 12.82C7.21248 12.6451 7.27739 12.4759 7.39663 12.3479L10.7446 8.99989L7.39663 5.65189C7.27022 5.52533 7.19922 5.35377 7.19922 5.17489C7.19922 4.99602 7.27022 4.82446 7.39663 4.69789Z" fill="#05070B"/>
+                            </svg>
+                        </div>
+                    </div>
+               </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .about-frame4-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: auto;
+        margin-bottom: 192px;
+    }
+
+    .about-frame4 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 0px;
+        gap: 72px;
+        width: 1440px;
+        height: 855px;
+        flex: none;
+        order: 3;
+        flex-grow: 0;
+    }
+
+    .about-frame4-header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 0px;
+        gap: 24px;
+        width: 568px;
+        height: 255px;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+
+    .about-frame4-header-title {
+        width: 568px;
+        height: 150px;
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 60px;
+        line-height: 125%;
+        text-align: center;
+        color: #05070B;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+
+    .about-frame4-header-title-highlight {
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 60px;
+        line-height: 125%;
+        text-align: center;
+        background: radial-gradient(58.32% 58.33% at 21.17% 12.5%, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0) 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */, #4B3CE4;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        background-clip: text;
+        text-fill-color: transparent;
+        background-blend-mode: plus-lighter, normal;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+
+    .about-frame4-header-subtitle {
+        width: 568px;
+        height: 81px;
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 150%;
+        text-align: center;
+        color: #71717A;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-items: flex-start;
+        align-content: flex-start;
+        padding: 0px;
+        gap: 24px;
+        width: 1440px;
+        height: 528px;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-title {
+        width: 1440px;
+        height: 45px;
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 36px;
+        line-height: 125%;
+        color: #05070B;
+        flex: none;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-card {
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding: 24px;
+        gap: 24px;
+        width: 342px;
+        height: 183px;
+        background: #F6F6F6;
+        border: 1px solid #E4E4E7;
+        border-radius: 12px;
+        flex: none;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-card-header {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 0px;
+        gap: 12px;
+        margin: 0 auto;
+        flex: none;
+        order: 0;
+        align-self: stretch;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-card-header-title {
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 125%;
+        color: #05070B;
+        flex: none;
+        order: 0;
+        align-self: stretch;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-card-header-subtitle {
+        width: 294px;
+        height: 24px;
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 150%;
+        color: #71717A;
+        flex: none;
+        order: 1;
+        align-self: stretch;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-card-link {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        width: 144px;
+        height: 24px;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+        cursor: pointer;
+    }
+
+    .about-frame4-content-card-link-text {
+        width: 122px;
+        height: 24px;
+        font-family: 'DM Sans';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 150%;
+        color: #05070B;
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+    }
+
+    .about-frame4-content-card-link-icon svg{
+        width: 18px;
+        height: 18px;
+        padding-top: 3.5px;
+        flex: none;
+        order: 1;
+        flex-grow: 0;
+    }
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,6 +2,7 @@
 import Frame1 from '~/components/about/Frame1.vue';
 import Frame2 from '~/components/about/Frame2.vue';
 import Frame3 from '~/components/about/Frame3.vue';
+import Frame4 from '~/components/about/Frame4.vue';
 
     definePageMeta({
         layout: 'custom'
@@ -12,4 +13,5 @@ import Frame3 from '~/components/about/Frame3.vue';
     <Frame1/>
     <Frame2/>
     <Frame3/>
+    <Frame4/>
 </template>
